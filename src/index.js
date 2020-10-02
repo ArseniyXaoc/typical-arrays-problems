2e32e1a6260e8998a91b59313b95418966712108
ArseniyXaoc <xaoc190789@gmail.com>
2020-10-02 19:55:26 +0300

    init: task typical arrey problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined){return 0};
+  if(array.length == 0){return 0};
+  let x = array;
+  x.sort((a, b) => a-b);
+  return x[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined){return 0};
+  if(array.length == 0){return 0};
+  let x = array;
+  x.sort((a, b) => a-b);  
+  return x[x.length-1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined){return 0};
+  if(array.length == 0){return 0};
+  let x = array;
+  const reduser = (item, count) => item + count;
+  let y = (x.reduce(reduser))/x.length;
+  console.log(y)
+  return y;
 }
